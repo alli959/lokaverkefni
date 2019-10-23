464f572414bb17d3a3941f3a307a54537fe45349
alli959 <alg35@hi.is>
2019-10-23 23:09:00 +0000

    color changes

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -7,14 +7,44 @@ import './navbar.css';
 
 
 class Navbar extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            colors: [
+            {background: 'black'},
+            {background: 'rgb(201, 40, 152)'},
+            {background: 'rgb(201, 40, 152)'}, 
+            {background: 'rgb(201, 40, 152)'}
+        ]
+
+        }
+    }
+    
+
+    changeColor(index){
+
+        const temp = this.state.colors;
+        for(let i = 0; i<temp.length; i++){
+            if(i === index){
+                temp[i] = {background: 'black'};
+            }
+            else{
+                temp[i] = {background: 'rgb(201, 40, 152)'};
+            }
+        }
+        this.setState({
+            colors: temp
+        });
+    };
     
     render(){
         return (
             <ul className="navBar">
-                <button><a className="Offers" href="#offers">Tilboð</a></button>
-                <button><a className="Burgers" href="#burgers">Borgarar</a></button>
-                <button><a className="Boats" href="#boats">Bátar</a></button>
-                <button><a className="Sandwiches" href="#sandwiches">Samlokur</a></button>
+                <button style = {this.state.colors[0]} onClick = {() => this.changeColor(0)}><a className="Offers" href="#offers">Tilboð</a></button>
+                <button style = {this.state.colors[1]} onClick = {() => this.changeColor(1)}><a className="Burgers" href="#burgers">Borgarar</a></button>
+                <button style = {this.state.colors[2]} onClick = {() => this.changeColor(2)}><a className="Boats" href="#boats">Bátar</a></button>
+                <button style = {this.state.colors[3]} onClick = {() => this.changeColor(3)}><a className="Sandwiches" href="#sandwiches">Samlokur</a></button>
             </ul>
 
         )
